bb4e578c0278f03a6d6b9401915823eac921dbe2
martinalanis <martinalanis.dev@gmail.com>
2021-03-22 15:46:30 -0600

    Creado endpoint para listado de productos

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/test', [AuthController::class, 'login']);
+
+Route::group(['middleware' => 'api'], function () {
+  Route::apiResource('products', ProductController::class);
+});
