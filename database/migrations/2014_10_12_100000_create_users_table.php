0559ad029c68a258fe7259842b5f2bb13ad547e5
martinalanis <martinalanis.dev@gmail.com>
2021-04-17 01:08:56 -0500

    Agregado campo telefono a modelo User

--- database/migrations/2014_10_12_100000_create_users_table.php
+++ database/migrations/2014_10_12_100000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->bigInteger('role_id')->unsigned();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->tinyInteger('status')->default(1);
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,12 +18,14 @@ class UserSeeder extends Seeder
       [
         'name'      =>  'Administrador',
         'email'     =>  'admin@zapateriadleon.com',
+        'phone'     =>  '4433123456',
         'role_id'   =>  1,
         'password'  =>  '123456'
       ],
       [
         'name'      =>  'proveedor',
         'email'     =>  'prov1@zapateriadleon.com',
+        'phone'     =>  '4433555555',
         'role_id'   =>  2,
         'password'  =>  '123456'
       ]
